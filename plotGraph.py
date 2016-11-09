093a615d30c66d4355e56883ddf8d336c0c8c2a9
tortuga90 <tyagi.himanshu90@gmail.com>
2016-11-09 00:10:34 -0700

    code for all three errors in one graph added

--- plotGraph.py
+++ plotGraph.py
@@ -11,41 +11,63 @@ import numpy as np
 #import pdb
 
 def plotGraphs():
-    ## plot training errors vs epochs
+    ## plot training errors vs epochs    
+#    savedParams = pickle.load(open('Error_dicr.pkl'))
+#    savedTrain = savedParams["train"]
+#    savedTrainArray = np.asarray(savedTrain)
+#    x = np.asarray(range(0,len(savedTrainArray)))
+#    plt.xlabel('Epochs')
+#    plt.ylabel('Training Error %')
+#    plt.title('Training Errors vs Epochs')
+#    plt.plot(x,savedTrainArray)
+#    plt.savefig('TrainingError.png')
+#    plt.show()
+#    
+#    ## plot validation error
+#    savedValid = savedParams["valid"]
+#    savedValidArray = np.asarray(savedValid)
+#    x = np.asarray(range(0,len(savedValidArray)))
+#    plt.xlabel('Epochs')
+#    plt.ylabel('Validation Error %')
+#    plt.title('Validation Errors vs Epochs')
+#    plt.plot(x,savedValidArray)
+#    plt.savefig('ValidationError.png')
+#    plt.show()
+#    
+#    ## plot test error
+#    savedTest = savedParams["test"]
+#    savedTestArray = np.asarray(savedTest)
+#    x = np.asarray(range(0,len(savedTestArray))) 
+#    plt.xlabel('Epochs')
+#    plt.ylabel('Test Error %')
+#    plt.title('Test Errors vs Epochs')
+#    plt.plot(x,savedTestArray)    
+#    plt.savefig('TestError.png')
+#    plt.show()
     
+    
+    ## code to plot all three in one graph
     savedParams = pickle.load(open('Error_dicr.pkl'))
     savedTrain = savedParams["train"]
     savedTrainArray = np.asarray(savedTrain)
     x = np.asarray(range(0,len(savedTrainArray)))
     plt.xlabel('Epochs')
-    plt.ylabel('Training Error %')
-    plt.title('Training Errors vs Epochs')
+    plt.ylabel('Error %')
+    plt.title('Errors vs Epochs')
     plt.plot(x,savedTrainArray)
-    plt.savefig('TrainingError.png')
-    plt.show()
     
-    ## plot validation error
     savedValid = savedParams["valid"]
     savedValidArray = np.asarray(savedValid)
     x = np.asarray(range(0,len(savedValidArray)))
-    plt.xlabel('Epochs')
-    plt.ylabel('Validation Error %')
-    plt.title('Validation Errors vs Epochs')
     plt.plot(x,savedValidArray)
-    plt.savefig('ValidationError.png')
-    plt.show()
     
-    ## plot test error
     savedTest = savedParams["test"]
     savedTestArray = np.asarray(savedTest)
-    x = np.asarray(range(0,len(savedTestArray)))  
-    plt.xlabel('Epochs')
-    plt.ylabel('Test Error %')
-    plt.title('Test Errors vs Epochs')
-    plt.plot(x,savedTestArray)    
-    plt.savefig('TestError.png')
+    x = np.asarray(range(0,len(savedTestArray)))
+    plt.plot(x,savedTestArray)
+    plt.legend(['Training Error','Validation Error', 'Test Error'],loc='top right')
+    plt.savefig('Error.png')
     plt.show()
-    
 
 if __name__ == '__main__':
     plotGraphs()
